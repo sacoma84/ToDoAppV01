c5fbc6552a50b34bfb0d97434fc87551bb1d43cc
DESKTOP-OVFGR4G\Marx <sacoma@gmx.de>
2025-12-21 11:03:41 +0100

    Initial Commit

---
+++ ToDoAppV01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoAppV01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
