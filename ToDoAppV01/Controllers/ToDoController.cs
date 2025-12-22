66ad8ad15fda386d3eb38c27d8be179d1445aa4a
DESKTOP-OVFGR4G\Marx <sacoma@gmx.de>
2025-12-22 11:19:25 +0100

    - Add new controller “ToDoController” - Add New view “ToDo/Index.cshtml” - Add new link to file “_Layout.cshtml” to access view “ToDo/Index.cshtml” - Add new button  to file “Home/Index.cshtml” with link to view “ToDo/Index.cshtml”

---
+++ ToDoAppV01/Controllers/ToDoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoAppV01.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
